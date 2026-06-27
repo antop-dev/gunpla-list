d8828af6c561c9f05375171747e4285f7d02249a
antop <antop@kakao.com>
2026-06-27 15:04:22 +0900

    "정적 리소스 캐싱을 위한 Cache-Control 설정 추가"

--- src/main/kotlin/ia/antop/gunpla/common/config/WebMvcConfig.kt
+++ src/main/kotlin/ia/antop/gunpla/common/config/WebMvcConfig.kt
@@ -1,19 +1,24 @@
 package ia.antop.gunpla.common.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.CacheControl
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.util.concurrent.TimeUnit
 
 @Configuration
 class WebMvcConfig(
     private val appProperties: AppProperties,
 ) : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        val cacheControl = CacheControl.maxAge(24, TimeUnit.HOURS)
         registry
             .addResourceHandler("/box-art/original/**")
             .addResourceLocations("file:${appProperties.boxArt.originalDirectory}/")
+            .setCacheControl(cacheControl)
         registry
             .addResourceHandler("/box-art/thumbnail/**")
             .addResourceLocations("file:${appProperties.boxArt.thumbnailDirectory}/")
+            .setCacheControl(cacheControl)
     }
 }
